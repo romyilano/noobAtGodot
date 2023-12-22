f5f2d67555ba24ada8b1fba81affa9fc68915b3f
romy <romy@snowyla.com>
2023-12-22 15:39:27 -0800

    added start and die functions

--- player.gd
+++ player.gd
@@ -8,7 +8,16 @@ var screensize = Vector2(480,720)
 func _ready():
 	pass # Replace with function body.
 
+func start():
+	set_process(true)
+	position = screensize / 2
+	$AnimatedSprite2D.animation = "idle"
 
+func die():
+	$AnimatedSprite2D.animation = "hurt"
+	# stop calling process() every frame
+	set_process(false)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	get_input()
