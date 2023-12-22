825d02dbea80991daaa779e1d83d2005ca49ea02
romy <romy@snowyla.com>
2023-12-22 11:36:35 -0800

    noobs why isn't the sprite working

--- player.gd
+++ player.gd
@@ -4,8 +4,6 @@ extends Area2D
 var velocity = Vector2()
 var screensize = Vector2(480,720)
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,4 +11,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	get_input()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+
+# classic 2D game controls
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	# note that velocity is normalized
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
