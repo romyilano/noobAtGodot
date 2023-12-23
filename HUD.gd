b7a495606ff263c00a94f4fa3258b2d4e9e30e7e
romy <romy@snowyla.com>
2023-12-22 17:37:21 -0800

    added script to margin container

---
+++ HUD.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+signal start_game
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func update_score(value):
+	$MarginContainer/Score.text = str(value)
+
+func update_timer(value):
+	$MarginContainer/Time.text = str(value)
