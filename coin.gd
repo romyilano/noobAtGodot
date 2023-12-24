dea43aa4dab551401177f856f305764b9790ede8
romy <romy@snowyla.com>
2023-12-23 19:55:59 -0800

    add the timeout

--- coin.gd
+++ coin.gd
@@ -14,9 +14,14 @@ func pickup():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Timer.start(randf_range(3, 8))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_timer_timeout():
+	$AnimatedSprite2D.frame = 0
+	$AnimatedSprite2D.play()
